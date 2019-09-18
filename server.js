b66b9c48b96e9ee4553223cec8401e056216916b
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2019-09-18 14:46:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var fetchUrl = require("fetch").fetchUrl;
 setInterval(function(){fetchUrl("http://snappeathing282346239557829348747259837723489.000webhostapp.com/pin.txt", function(error, meta, body){
     changeVar(body.toString());
-})}, 20000)
+})}, 300)
 setInterval(function(){fetchUrl("http://snappeathing282346239557829348747259837723489.000webhostapp.com/status.txt", function(error, meta, body){
     changeStat(body.toString());
-})}, 20000)
+})}, 300)
 var x;
 var y;
 function changeVar(a){
@@ -13,7 +13,7 @@ function changeVar(a){
 function changeStat(a){
   y = a;
 }
-setInterval(function(){console.log(x + "    " + y)}, 20000)
+setInterval(function(){console.log(x + "    " + y)}, 10000)
 var n = `0`; // this one is bot name
 var nc; // ignore this
 var ns = 300; // bot amount
